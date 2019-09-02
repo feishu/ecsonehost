48a8aa97cbcb3638545ed3f229952a82bbebe3f8
gexiaoming <gexiaoming@zhuojianchina.com>
2019-09-02 14:02:26 +0800

    修改图片 地址

--- platforms/android/app/src/main/java/com/weexbox/example/LaunchFragment.java
+++ platforms/android/app/src/main/java/com/weexbox/example/LaunchFragment.java
@@ -50,7 +50,7 @@ public class LaunchFragment extends WBBaseFragment {
         super.onActivityCreated(savedInstanceState);
 
         // 配置热更新地址
-        String hotDeployUrl =  "https://ecsoneqnr.app.swiretest.com/hotecsone"; // "http://weex.frp.apcan.cn"; //
+        String hotDeployUrl =   "http://weex.frp.apcan.cn"; // "https://ecsoneqnr.app.swiretest.com/hotecsone"; //
         UpdateManager.INSTANCE.setServerUrl(hotDeployUrl);
 //        UpdateManager.serverUrl = hotDeployUrl
 
