e5a8c0ca12273b3830eb125606aea3da695b4aa3
lezix <5344922+lezix@user.noreply.gitee.com>
2020-06-11 11:39:30 +0800

    update

--- platforms/android/app/src/main/java/com/weexbox/example/modules/LELModule.java
+++ platforms/android/app/src/main/java/com/weexbox/example/modules/LELModule.java
@@ -292,7 +292,7 @@ public class LELModule extends WXModule {
     @JSMethod
     public void launchValidationApp(JSONObject jsonObject, JSCallback successCallBack, JSCallback errorCallBack, JSCallback completeCallBack){
         ModuleAdapterCallBack moduleAdapterCallBack = new ModuleAdapterCallBack(successCallBack, errorCallBack, completeCallBack);
-        boolean b = ValidationUtils.launchValidationApp(mWXSDKInstance.getContext(),UserName,jsonObject.getString("password"),jsonObject.getString("sfaUserId"));
+        boolean b = ValidationUtils.launchValidationApp(mWXSDKInstance.getContext(),UserName,jsonObject.getString("pwd"),jsonObject.getString("uid"));
         if(b){
             moduleAdapterCallBack.success("");
         }else{
