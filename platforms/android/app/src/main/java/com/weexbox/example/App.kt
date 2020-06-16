fcf4ff5c8dcdbc85fd1b5cff85fa262a857509d9
lezix <5344922+lezix@user.noreply.gitee.com>
2020-06-16 16:41:19 +0800

    update

--- platforms/android/app/src/main/java/com/weexbox/example/App.kt
+++ platforms/android/app/src/main/java/com/weexbox/example/App.kt
@@ -3,6 +3,7 @@ package com.weexbox.example
 import android.app.Application
 import com.lelibrary.androidlelibrary.init.SDKInsigma
 import com.taobao.weex.WXSDKEngine
+import com.weex.weexextra.Global
 import com.weexbox.core.WeexBoxEngine
 import com.weexbox.example.modules.LELModule
 import com.weexbox.example.modules.SmartTagModule
@@ -20,6 +21,7 @@ class App : Application() {
         SDKInsigma.init(this,true)
         WXSDKEngine.registerModule("SmartTag", SmartTagModule::class.java)
         WXSDKEngine.registerModule("event", EventModule::class.java)
+        Global.init(this)
     }
 
 }
